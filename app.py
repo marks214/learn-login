34b20f402ef6a3ab9a86fcdb53918c4d8b844961
marks214 <aimee.oz.x@gmail.com>
2021-01-30 21:47:46 -0800

    session for next is working

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request, session, redirect
 from flask_login import LoginManager, UserMixin, login_user, login_required, current_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
+from urllib.parse import urlparse, urljoin
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Thisisasecret!'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///login_database.db'
+# this should take the user to the page they were trying to access after they login
+app.config['USE_SESSION_FOR_NEXT'] = True
 
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
+# this redirects the user to the login screen if they are not logged in
+login_manager.login_view = 'login'
+# set a person message, otherwise there is a default
+login_manager.login_message = 'You can\'t access that page, you need to login first'
+
+def is_safe_url(target):
+  ref_url = urlparse(request.host_url)
+  test_url = urlparse(urljoin(request.host_url, target))
+  
+  # is this a url on my server? if it is, redirect, if not, don't
+  return test_url.scheme in ('http', 'https') and ref_url.netloc == test_url.netloc
 
 class User(UserMixin, db.Model):
   id = db.Column(db.Integer, primary_key=True)
@@ -19,12 +33,27 @@ def load_user(user_id):
   return User.query.get(int(user_id))
   
 # route where user can login:
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-  user = User.query.filter_by(username='marks214').first()
-  # create a session based off the user, put the user id in the session, so when you visit different pages the user loader will look for the user id
-  login_user(user)
-  return f'<h1>{str(user)} logged in</h1>'
+  if request.method == 'POST':
+    username = request.form['username']
+    user = User.query.filter_by(username=username).first()
+
+    if not user:
+      return 'User does not exist!'
+    
+    login_user(user)
+
+    if 'next' in session:
+      next = session['next']
+
+      if is_safe_url(next):
+        return redirect(next)
+    
+    return '<h1>Your are now logged in!</h1>'
+  
+  # session['next'] = request.args.get('next')
+  return render_template('login.html')
 
 @app.route('/home')
 @login_required
